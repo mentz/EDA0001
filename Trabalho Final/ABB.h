3c748b4938ab66c31d954c1edd659eb86cb00219
Mentz <Mentz@users.noreply.github.com>
2017-06-16 21:23:34 -0300

    Update ABB.h

--- Trabalho Final/ABB.h
+++ Trabalho Final/ABB.h
@@ -2,7 +2,6 @@
 #include "stdlib.h"
 #include "string.h"
 
-
 #define SIM 1
 #define NAO 0
 #define MAIOR
@@ -14,27 +13,27 @@
 typedef struct ABB *pABB,**ppABB;
 
 /*
-operação de criação:
-pré-condição: a existência de um apontador adequadamente
-declarado e passado por referência para a função juntamente com
-o tamanho (a quantidade de bytes) do tipo de informação a ser manipulada.
-pós-condição: a criação e inicialização do TDA ABB, atribuição do seu endereço ao
-ponteiro passado por referencia. Na ocorrência de algum problema durante a
-criação retorna-se a macro FRACASSO, caso contrário retorna-se a macro SUCESSO.*/
+operaÃ§Ã£o de criaÃ§Ã£o:
+prÃ©-condiÃ§Ã£o: a existÃªncia de um apontador adequadamente
+declarado e passado por referÃªncia para a funÃ§Ã£o juntamente com
+o tamanho (a quantidade de bytes) do tipo de informaÃ§Ã£o a ser manipulada.
+pÃ³s-condiÃ§Ã£o: a criaÃ§Ã£o e inicializaÃ§Ã£o do TDA ABB, atribuiÃ§Ã£o do seu endereÃ§o ao
+ponteiro passado por referencia. Na ocorrÃªncia de algum problema durante a
+criaÃ§Ã£o retorna-se a macro FRACASSO, caso contrÃ¡rio retorna-se a macro SUCESSO.*/
 int criaABB(ppABB pp, int tamInfo);
 
-/*operação destrutora:
-pré-condição: a existência de um apontador declarado externamente a esta função,
-devidamente iniciado com o endereço do TDA e cujo endereço (deste apontador)
-deve ser passado para a função.
-pós-condição: a liberação de qualquer região de memória utilizada pelo TDA
-seguida da anulação do ponteiro passado por referencia*/
+/*operaÃ§Ã£o destrutora:
+prÃ©-condiÃ§Ã£o: a existÃªncia de um apontador declarado externamente a esta funÃ§Ã£o,
+devidamente iniciado com o endereÃ§o do TDA e cujo endereÃ§o (deste apontador)
+deve ser passado para a funÃ§Ã£o.
+pÃ³s-condiÃ§Ã£o: a liberaÃ§Ã£o de qualquer regiÃ£o de memÃ³ria utilizada pelo TDA
+seguida da anulaÃ§Ã£o do ponteiro passado por referencia*/
 void destroiABB(ppABB pp);
 
-/* tipo de operação: função de manipulação.
-pré condição: a pré existência do TDA cujo endereço é passado para a função.
-pós condição: esvaziamento dos dados, o TDA retorna ao estado apresentado
-imediatamente após a sua criação. */
+/* tipo de operaÃ§Ã£o: funÃ§Ã£o de manipulaÃ§Ã£o.
+prÃ© condiÃ§Ã£o: a prÃ© existÃªncia do TDA cujo endereÃ§o Ã© passado para a funÃ§Ã£o.
+pÃ³s condiÃ§Ã£o: esvaziamento dos dados, o TDA retorna ao estado apresentado
+imediatamente apÃ³s a sua criaÃ§Ã£o. */
 void reiniciaABB(pABB p);
 
 //void apagaNoABB(pNoABB p);
